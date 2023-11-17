73db3e10bd2dd9679702e7db40e38c3e63e5400f
Shola Oshodi <sholaoshodi.00@gmail.com>
2023-11-17 11:03:21 +0100

    Smtlib refactor bash

--- SMT/src/binary.sh
+++ SMT/src/binary.sh
@@ -15,11 +15,12 @@ o_variable=$2
 o_value=$3
 l_bound=$4
 
+
 o_variable_lower_bound="(assert (>= $o_variable $l_bound))"
 o_variable_upper_bound="(assert (<= $o_variable $o_value))"
 check_sat="(check-sat)"
 get_value="(get-value ($o_variable))"
-
+# write constraints on file 
 {
     echo "$o_variable_lower_bound"
     echo "$o_variable_upper_bound" 
@@ -27,6 +28,7 @@ get_value="(get-value ($o_variable))"
     echo "$get_value" 
 } >> "$file"
 
+# binary search 
 u_bound=$o_value
 bound_difference=$((u_bound - l_bound))
 distance=$((bound_difference / 2))
@@ -42,8 +44,8 @@ do
     while read text;
     do
     : '
-     if the solution is unsat and not other 
-     feasible solution has been found -> exit
+     if the solution is unsat and no
+     feasible solutions have been found -> exit
 
     '
 
@@ -52,13 +54,13 @@ do
         break
     fi
 
-
-     one_sat=1
+     one_sat=1 #if it's sat at least one time
      case $text in
         sat)   sat=1;;
         unsat) sat=0;;
         esac
-
+    
+    #bound update
      if [ "$bound_difference" -eq 1 ]; then
         middle=$l_bound
      else
@@ -70,7 +72,7 @@ do
      fi
 
 
-
+    # ignored during the first loop 
     if [[ $first -eq 1 ]]; then
         if [[ $sat -eq 1 ]]; then
             o_value=$(echo "$text" | sed 's/[^0-9]*//g')
@@ -79,12 +81,15 @@ do
             distance=$((bound_difference / 2))
             middle=$((u_bound - distance))
         else
+        # if unsat or unknown
+            #update bound
             l_bound=$middle
             bound_difference=$((u_bound - l_bound))
             distance=$((bound_difference / 2))
             middle=$((u_bound - distance))
             fi
 
+            #add constraint
             assert_statement="(assert (<= $o_variable $middle))"
             check_sat="(check-sat)"
             get_value="(get-value ($o_variable))"
@@ -97,7 +102,7 @@ do
             
   else
    first=1
-
+   #Delete previously added lines
         if [[ $sat -eq 0 ]]; then
             sed -i '$ d' $file
         fi
@@ -116,34 +121,42 @@ done
 
 
    
-
+#if a sat solution exists
 if [[ $one_sat -eq 1 ]]; then
     sed -i '$ d' $file 
     sed -i '$ d' $file 
     sed -i '$ d' $file 
     echo $o_value
+
     for ((i=0; i<couriers; i++)); do
     for ((j=0; j<packages; j++)); do
+
           declare_fun="(declare-fun path${i}_${j} () Int)"
           assert_equal="(assert (= (select asg${i} ${j}) path${i}_${j}))"
-
-            {
-                echo "$declare_fun" 
-                echo "$assert_equal" 
+          {
+            echo "$declare_fun" 
+            echo "$assert_equal" 
+            
             } >> "$file"
-           done 
+
+        done 
     done
     echo "(check-sat)" >> $file
 
+    # get result 
     for ((i=0; i<couriers; i++)); do
     for ((j=0; j<packages; j++)); do
+
         echo "(get-value (path$i"_"$j))">> $file
+    
     done
     done
+
      while read text; do
-  	if [[ $text != 'sat' ]]; then
-	    echo $text  
-  	fi
+     
+        if [[ $text != 'sat' ]]; then
+            echo $text  
+  	    fi
   
     done < <("${solver[@]}" <"$file")
 else
